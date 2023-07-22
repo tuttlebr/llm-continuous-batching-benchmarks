44fedbbc1cb07fc8354910ce8389f5bb4caa1a78
Brandon Tuttle <btuttle@nvidia.com>
2023-07-22 08:35:05 -0700

    updated latency test parms

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 set -o pipefail
-/benchmarking/benchmark_configs/vllm_variable_size
+# /benchmarking/benchmark_configs/vllm_variable_size
 /benchmarking/benchmark_configs/vllm_variable_size_latency
